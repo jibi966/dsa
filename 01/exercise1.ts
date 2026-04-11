52b10619f7ed065ef8449f98e5a3ea3fbed53de6
Jibin <jibin@empplan.com>
2026-04-11 19:54:09 +0530

    first commit

---
+++ 01/exercise1.ts
@@ -0,0 +1,25 @@
+// return if 2 array containes the same elements
+//  [1,2,3] and [3,6,5] should return true
+// [1,2,3] and [6,5] should return false
+
+
+
+function containCommonItem(arr1: number[], arr2: number[]): boolean {
+    const foundMap: Record<number, boolean> = {};
+
+    for (const item of arr1) {
+        foundMap[item] = true;
+    };
+
+    for (const item of arr2) {
+        if (foundMap[item]) {
+            return true;
+        }
+    }
+
+
+    return false;
+}
+
+console.log(containCommonItem([1,2,3], [3,6,5])); // should return true
+console.log(containCommonItem([1,2,3], [6,5])); // should return false
